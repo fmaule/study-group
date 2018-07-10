b2edb672cb65f077e7dd405c40b5ed8f37346b42
Fernando A. Maule <fernando.maule@tesglobal.com>
2018-07-10 18:00:14 +0200

    Add for/loop and benchmark with reduce

--- promises-async-await/async-reduce-and-for-loop.js
+++ promises-async-await/async-reduce-and-for-loop.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-syntax */
+
 const { promisifiedWithUtilTimeout } = require('./promisify.js');
 
 const doSomethingAsync = async (value) => {
@@ -12,7 +14,7 @@ const doSomethingElseAsync = async (value) => {
 
 const arrayToReduce = ['value1', 'value2', 'value3', 'value4'];
 
-const doIt = async () => {
+const doReduce = async () => {
   const asyncReducer = async (prevPromise, current) => {  // the reducer will run sequentially
     const results = await prevPromise;
     const processedValue = await doSomethingAsync(current);
@@ -30,8 +32,35 @@ const doIt = async () => {
 
   const reduced = await arrayToReduce.reduce(asyncReducer, Promise.resolve([]));
 
-  console.log('input:\n', arrayToReduce, '\noutput:\n', reduced);
+  console.log('reduce\ninput:\n', arrayToReduce, '\noutput:\n', reduced);
 };
 
-doIt()
+
+const doForLoop = async () => {
+  const results = [];
+  for (const current of arrayToReduce) {
+    const processedValue = await doSomethingAsync(current); // eslint-disable-line no-await-in-loop
+    const superProcessedValue = await doSomethingElseAsync(current); // eslint-disable-line no-await-in-loop
+
+    results.push({
+      [current]:
+      {
+        standard: processedValue,
+        super: superProcessedValue,
+      },
+    });
+  }
+
+  console.log('for/loop\ninput:\n', arrayToReduce, '\noutput:\n', results);
+};
+
+console.time('doReduce');
+doReduce()
+  .then(() => console.timeEnd('doReduce'))
+  .catch(e => console.log('ERROR!', e));
+
+
+console.time('doForLoop');
+doForLoop()
+  .then(() => console.timeEnd('doForLoop'))
   .catch(e => console.log('ERROR!', e));
